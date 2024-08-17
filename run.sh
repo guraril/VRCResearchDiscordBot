06257a4a34f9a629b3d84328d5f72c7789dc0ca3
guraril <guraril0716@gmail.com>
2024-08-17 12:34:26 +0900

    Fixes #2

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 python3 -m venv celestnode_discord_bot_venv
+./celestnode_discord_bot_venv/bin/pip install schedule
+./celestnode_discord_bot_venv/bin/pip install --upgrade pip
 ./celestnode_discord_bot_venv/bin/python3 main.py
 # clear virtual environment
 rm -rf celestnode_discord_bot_venv
